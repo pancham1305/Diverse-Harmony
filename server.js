51c4de4cf302b27498f1ca0511c8e2b1a2acf3c8
pancham1305 <pancham1305@gmail.com>
2022-06-18 21:00:27 +0530

    Commit2

--- server.js
+++ server.js
@@ -9,7 +9,17 @@ app.use(express.static(__dirname + "/assets"));
 app.get("/", (req, res) => {
   res.render(__dirname + "/public/index.ejs");
 });
+app.get("/Music-Dance", (req, res) => {
+  res.render(__dirname + "/public/music_dance.ejs");
+});
 
-app.listen(7500, () => {
+app.get("/Food", (req, res) => {
+  res.render(__dirname + "/public/food.ejs");
+});
+app.get("/Festivals", (req, res) => {
+  res.render(__dirname + "/public/festivals.ejs");
+});
+
+app.listen(process.env.PORT || 7500, () => {
   console.log("Server is listening at 7500");
 });
